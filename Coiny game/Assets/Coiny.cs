0164dc3e4ad65db1dad36e0416f620bbff5bb576
Thesebio <coveromaki@gmail.com>
2018-01-24 11:59:37 +0100

    Da coiny
    Who will win?
    A multimedia engineer
    OR
    A coiny sound

---
+++ Coiny game/Assets/Coiny.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Coiny : MonoBehaviour {
+    public static int CoinCount = 0;
+    public static int CoinCountInitial = 0;
+
+    public static int CoinWin = 10;
+    public GameObject FirstLine1CurrentCoins;
+    public GameObject FirstLine2TotalCoins;
+
+    // Use this for initialization
+    void Start () {
+
+        Debug.Log("Coin created");
+        ++Coiny.CoinCount;
+        ++Coiny.CoinCountInitial;
+
+
+        FirstLine1CurrentCoins = GameObject.Find("FirstLine1CurrentCoins");
+        FirstLine2TotalCoins = GameObject.Find("FirstLine2TotalCoins");
+
+        FirstLine2TotalCoins.GetComponent<Text>().text = Coiny.CoinCountInitial.ToString();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+
+
+        if (other.CompareTag("Player")){
+            Debug.Log("Le recogido");
+            GetComponent<AudioSource>().Play();
+
+            Destroy(gameObject);
+        }
+
+    }
+    //called when destroy
+    void OnDestroy()
+    {
+        //decrement coins
+        --Coiny.CoinCount;
+
+        Debug.Log("Quedan " + Coiny.CoinCount + " Monedas de " + Coiny.CoinCountInitial);
+
+        FirstLine1CurrentCoins.GetComponent<Text>().text = (Coiny.CoinCountInitial - Coiny.CoinCount).ToString();
+
+        //check remaining coins
+        if (Coiny.CoinCount <= Coiny.CoinCountInitial - CoinWin)
+        {
+            //da win
+            Debug.Log("Le win");
+
+            GameObject Timer = GameObject.Find("Level Timer");
+            Destroy(Timer);
+
+            GameObject[] FireworkSystem =
+                GameObject.FindGameObjectsWithTag("Fireworks");
+            foreach(GameObject GO in FireworkSystem)
+            {
+                GO.GetComponent<ParticleSystem>().Play();
+            }
+        }
+    }
+}
---
+++ Coiny game/Assets/Timer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+    //max time to complete level (seconds)
+    public float MaxTime = 60f;
+    public float StartTime = 0f;
+    public float CurrentTime = 0f;
+
+    public GameObject TextTime;
+    int min;
+    int s;
+    //countdown
+    [SerializeField]
+    private float Countdown = 0;
+	// Use this for initialization
+	void Start () {
+        Countdown = MaxTime;
+        TextTime = GameObject.Find("SecondLineTime");
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //reduce time
+        Countdown -= Time.deltaTime;
+        CurrentTime += Time.deltaTime;
+
+        min = (int)CurrentTime / 60;
+        s = (int) CurrentTime - (min * 60);
+
+        TextTime.GetComponent<Text>().text = min.ToString("D2") + " : " + s.ToString("D2");
+        //restart level if time runs out
+        /*        if(Countdown <= 0)
+              {
+                    //ForcedReset coin count
+                    Coiny.CoinCount = 0;
+                    Application.LoadLevel(Application.loadedLevel); //Habra que cambiarlo
+
+                }
+        */
+    }
+}
